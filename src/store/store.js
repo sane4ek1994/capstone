e50a1050526ebb49c6b67617e4f3e94f3e8a80d9
Alexandr Naum <mfq07k9cz@gmail.com>
2022-11-16 13:24:29 +0300

    remove middlewre directory and window?+

--- src/store/store.js
+++ src/store/store.js
@@ -16,7 +16,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+  (process.env.NODE_ENV !== 'production' && window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
