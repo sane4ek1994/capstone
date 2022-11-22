6274e7d5f2620690551ea2bed2792ee0339f12d1
Alexandr Naum <mfq07k9cz@gmail.com>
2022-11-22 12:21:54 +0300

    cart store to ts

--- src/store/categories/categories.action.ts
+++ src/store/categories/categories.action.ts
@@ -1,17 +1,19 @@
 import { CATEGORIES_ACTION_TYPES, Category } from './categories.types'
-import { createAction, Action, ActionWithPayload } from '../../utils'
+import { createAction, Action, ActionWithPayload, withMatcher } from '../../utils'
 
 export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>
 export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, Category[]>
 export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>
 
-export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFailed
+export const fetchCategoriesStart = withMatcher(
+  (): FetchCategoriesStart => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+)
 
-export const fetchCategoriesStart = (): FetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray: Category[]): FetchCategoriesSuccess =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray)
+)
 
-export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray)
-
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+export const fetchCategoriesFailed = withMatcher(
+  (error: Error): FetchCategoriesFailed => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+)
