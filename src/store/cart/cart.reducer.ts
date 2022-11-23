238fa58a1e30b9215c70e5ebda41062463fb7f48
Alexandr Naum <mfq07k9cz@gmail.com>
2022-11-23 16:22:40 +0300

    update store ts

--- src/store/cart/cart.reducer.ts
+++ src/store/cart/cart.reducer.ts
@@ -7,7 +7,7 @@ export type CartState = {
   readonly cartItems: CartItem[]
 }
 
-export const CART_INITIAL_STATE = {
+export const CART_INITIAL_STATE: CartState = {
   isCartOpen: false,
   cartItems: []
 }
